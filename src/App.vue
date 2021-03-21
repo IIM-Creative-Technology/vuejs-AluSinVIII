195e66323f84939ffa4b9404a12fdc28f550ad30
AluSinVIII <70762378+AluSinVIII@users.noreply.github.com>
2021-03-21 16:33:50 +0100

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+        <Admin></Admin>
+    <img src="./assets/logo.png">
+
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import Admin from './components/Admin.vue'
+export default {
+  name: 'app',
+  components: {
+    Admin
+    
+  },
+  data () {
+    return {
+      msg: 'Test'
+    }
+
+  }
+  
+
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Admin.vue
@@ -0,0 +1,19 @@
+<!-- ./component/Admin.vue -->
+
+<template>
+<p>Test</p>
+</template>
+
+<script>
+
+export default {
+    name: "Admin",
+    data() {
+        
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
